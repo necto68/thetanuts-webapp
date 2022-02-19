b090f47635a14a90c2f9a279717a6a72741fd109
Ethan So <ethan.so.wai.yein@gmail.com>
2022-02-19 17:31:43 +0800

    fix: prop

--- src/features/root/components/Header.tsx
+++ src/features/root/components/Header.tsx
@@ -1,11 +1,12 @@
 import { ChainSelect, WalletButton } from "../../wallet/components";
+import { chains } from "../../wallet/constants/chains";
 
 import { HeaderContainer, ButtonsContainer } from "./Header.styles";
 
 export const Header = () => (
   <HeaderContainer>
     <ButtonsContainer>
-      <ChainSelect />
+      <ChainSelect selectedChains={chains} />
       <WalletButton />
     </ButtonsContainer>
   </HeaderContainer>
