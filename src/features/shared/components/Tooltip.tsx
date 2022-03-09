f893b3c97047d347b9b488c29cadfc364b9ea3b7
Greg G Tan <greggtan99@gmail.com>
2022-03-09 08:34:40 +0800

    fixed all issues

--- src/features/shared/components/Tooltip.tsx
+++ src/features/shared/components/Tooltip.tsx
@@ -1,47 +1,57 @@
-import ReactTooltip from 'react-tooltip';
+import ReactTooltip from "react-tooltip";
 import type { FC } from "react";
+
 import {
-    TooltipContainer,
-    TableTooltipHeader,
-    TableTooltipContent,
-    InfoIconDiv,
-    InfoIconDivPopup
+  TooltipContainer,
+  TableTooltipHeader,
+  TableTooltipContent,
+  InfoIconDiv,
+  InfoIconDivPopup,
 } from "./Tooltip.styles";
 
 interface TooltipProps {
-    toolTipId:string;
-    data?:string; 
-    type?:string;
-    WPY?:string;
-    MPY?:string;
-    APR?:string;
-    APY?:string;
+  toolTipId: string;
+  data?: string;
+  type?: string;
+  WPY?: string;
+  MPY?: string;
+  APR?: string;
+  APY?: string;
 }
 
-export const Tooltip: FC<TooltipProps> = ({toolTipId, data, type, WPY, MPY, APR, APY}) => {
-  return (
-      <>
-          {type==='table'? 
-            <>
-                <InfoIconDiv data-tip data-for={toolTipId} />
-                <ReactTooltip id="tableToolTip" place="right" type="light" effect="solid">
-                    <TableTooltipHeader>Extrapolated yields</TableTooltipHeader>
-                    <TableTooltipContent><strong>{WPY}%</strong> WPY (7-days)</TableTooltipContent>
-                    <TableTooltipContent><strong>{MPY}%</strong> MPY (monthly)</TableTooltipContent>
-                    <TableTooltipContent><strong>{APR}%</strong> APR (annually)</TableTooltipContent>
-                    <TableTooltipContent><strong>{APY}%</strong> APY (compounded)</TableTooltipContent>
-                </ReactTooltip>
-            </> 
-            :
-            <>
-                <InfoIconDivPopup data-tip data-for={toolTipId} />
-                <ReactTooltip id={toolTipId} place="right" type="light" effect="solid" >
-                    <TooltipContainer>
-                        {data}
-                    </TooltipContainer>
-                </ReactTooltip>
-            </>
-            }
-      </>
+export const Tooltip: FC<TooltipProps> = ({
+  toolTipId,
+  data,
+  type,
+  WPY,
+  MPY,
+  APR,
+  APY,
+}) =>
+  type === "table" ? (
+    <>
+      <InfoIconDiv data-for={toolTipId} data-tip />
+      <ReactTooltip effect="solid" id="tableToolTip" place="right" type="light">
+        <TableTooltipHeader>Extrapolated yields</TableTooltipHeader>
+        <TableTooltipContent>
+          <strong>{WPY}%</strong> WPY (7-days)
+        </TableTooltipContent>
+        <TableTooltipContent>
+          <strong>{MPY}%</strong> MPY (monthly)
+        </TableTooltipContent>
+        <TableTooltipContent>
+          <strong>{APR}%</strong> APR (annually)
+        </TableTooltipContent>
+        <TableTooltipContent>
+          <strong>{APY}%</strong> APY (compounded)
+        </TableTooltipContent>
+      </ReactTooltip>
+    </>
+  ) : (
+    <>
+      <InfoIconDivPopup data-for={toolTipId} data-tip />
+      <ReactTooltip effect="solid" id={toolTipId} place="right" type="light">
+        <TooltipContainer>{data}</TooltipContainer>
+      </ReactTooltip>
+    </>
   );
-};
