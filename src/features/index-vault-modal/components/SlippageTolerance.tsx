5b787570801d18f50347d389c088a6fa0bcee3e7
necto68 <necto68necto68@gmail.com>
2022-04-20 17:08:46 +0300

    refactor: slippage tolerance component

--- src/features/index-vault-modal/components/SlippageTolerance.tsx
+++ src/features/index-vault-modal/components/SlippageTolerance.tsx
@@ -2,10 +2,16 @@ import type { ChangeEvent } from "react";
 import { useCallback } from "react";
 import Big from "big.js";
 
-import { Tooltip } from "../../shared/components";
+import { IconContainer, Tooltip } from "../../shared/components";
 import { useSwapRouterState } from "../hooks";
+import { Warning } from "../icons";
 
-import { Container, Input, Title } from "./SlippageTolerance.styles";
+import {
+  Container,
+  InputContainer,
+  Input,
+  Title,
+} from "./SlippageTolerance.styles";
 
 export const SlippageTolerance = () => {
   const {
@@ -24,30 +30,28 @@ export const SlippageTolerance = () => {
   const slippageTolerance = new Big(slippageToleranceValue);
   const isError = slippageTolerance.lt(0.1) || slippageTolerance.gt(1);
 
-  if (isError) {
-    return (
-      <Tooltip
-        content="Slippage Tolerance should be between 0.1% and 1%"
-        id="slippageTolerance"
-        place="top"
-        root={
-          <Container>
-            <Input
-              isError
-              onChange={handleInputChange}
-              value={slippageToleranceInputValue}
-            />
-            <Title isError>%</Title>
-          </Container>
-        }
-      />
-    );
-  }
-
   return (
     <Container>
-      <Input onChange={handleInputChange} value={slippageToleranceInputValue} />
-      <Title>%</Title>
+      {isError ? (
+        <Tooltip
+          content="Slippage Tolerance should be between 0.1% and 1%"
+          id="slippageTolerance"
+          place="top"
+          root={
+            <IconContainer height={14} width={16}>
+              <Warning />
+            </IconContainer>
+          }
+        />
+      ) : null}
+      <InputContainer>
+        <Input
+          isError={isError}
+          onChange={handleInputChange}
+          value={slippageToleranceInputValue}
+        />
+        <Title>%</Title>
+      </InputContainer>
     </Container>
   );
 };
