bc9ba254bfe8df7fc169f24d8d31393637b51556
necto68 <necto68necto68@gmail.com>
2022-10-25 16:28:30 +0300

    fix: collateralAssetId in useLendingMarketModalConfig

--- src/features/lending-market-vault-modal/hooks/useLendingMarketModalConfig.ts
+++ src/features/lending-market-vault-modal/hooks/useLendingMarketModalConfig.ts
@@ -1,6 +1,7 @@
 import { useVaultModalState } from "../../modal/hooks";
 import { useCollateralAsset } from "../../lending-market/hooks";
 import { useLendingMarketVaultReader } from "../../lending-market-vault/hooks";
+import { collateralAssets } from "../../lending-market/constants";
 
 export const useLendingMarketModalConfig = () => {
   const [{ vaultId }] = useVaultModalState();
@@ -9,7 +10,9 @@ export const useLendingMarketModalConfig = () => {
 
   const { data } = lendingMarketVaultReaderQuery;
   const { collateralAsset } = data ?? {};
-  const { id: collateralAssetId = "" } = collateralAsset ?? {};
+
+  const { id: collateralAssetId = collateralAssets[0].id } =
+    collateralAsset ?? {};
 
   const collateralAssetQuery = useCollateralAsset(collateralAssetId);
 
