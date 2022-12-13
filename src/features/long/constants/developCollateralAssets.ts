e6a51db9a484d9587393556d09a4b9f8f385a316
necto68 <necto68necto68@gmail.com>
2022-12-13 16:57:53 +0200

    fix: add collateral asset for production

--- src/features/long/constants/developCollateralAssets.ts
+++ src/features/long/constants/developCollateralAssets.ts
@@ -13,15 +13,4 @@ export const developCollateralAssets: CollateralAssetConfig[] = [
         "0xeCD22E8924620fa5991267a5c65B57F4a982Eb15",
     },
   },
-  {
-    id: "USDC",
-
-    source: {
-      chainId: ChainId.ETHEREUM,
-      collateralAssetAddress: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
-
-      lendingPoolAddressesProviderAddress:
-        "0xC965590E1Ca4a1031Fc3B97771C1B98CD4C24182",
-    },
-  },
 ];
