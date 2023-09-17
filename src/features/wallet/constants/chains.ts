a49dc4612a3d7c70ec7f945fbc23e1dc90b23662
Brendan Zhong <brendan@de-fi.capital>
2023-09-17 12:32:58 +1000

    fix boba rpc

--- src/features/wallet/constants/chains.ts
+++ src/features/wallet/constants/chains.ts
@@ -210,7 +210,7 @@ export const chains: ChainConfig[] = [
     symbol: "ETH",
 
     urls: {
-      rpc: "https://boba-mainnet.gateway.pokt.network/v1/lb/623ad21b20354900396fed7f",
+      rpc: "https://mainnet.boba.network",
       explorer: "https://bobascan.com/",
       explorerApi: "https://api.bobascan.com/",
     },
